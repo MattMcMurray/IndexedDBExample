1fc2275e417c2227c77ffbd314131d60f2c42876
Matt McMurray <mathieumcmurray@gmail.com>
2018-06-28 20:39:02 -0500

    Show persisted 'goals' on page load and append new ones to the list

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,16 @@
     <h1>{{ msg }}</h1>
     <h2>DB is <span v-if="this.dbOpen">open</span><span v-else>closed</span></h2>
     <h3 v-show="objectStoresReady">Object stores are ready</h3>
-    <input type="text" name="goal" id="goalInput" v-model="goal.goalName">
-    <button v-on:click="addGoal()">Add Customer</button>
+    <form @submit.prevent="addGoal()">
+      <input type="text" name="goal" id="goalInput" v-model="goal.goalName">
+      <button type="submit">Add Goal</button>
+    </form>
+    <hr>
+    <ul>
+      <li v-for="goal in thisWeeksGoalsSorted">
+        {{ goal.goalName }} - {{ goal.goalDate }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -21,12 +29,28 @@ export default {
       goal: {
         goalName: "",
         goalDate: new Date()
-      }
+      },
+      thisWeeksGoals: []
+    }
+  },
+
+  computed: {
+    thisWeeksGoalsSorted: function () {
+      /**
+       * Sort goals by date in descending order.
+       *
+       * Adapted from this answer: https://stackoverflow.com/a/10124053
+       */
+      return this.thisWeeksGoals.sort(function(a,b){
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return new Date(b.goalDate) - new Date(a.goalDate);
+      });
     }
   },
 
   mounted: function (params) {
-    let request = window.indexedDB.open("MyTestDatabase", 2);
+    let request = window.indexedDB.open("GoalKeeperDatabase", 1);
     request.onerror = this.handleDBOpenError;
     request.onsuccess = this.handleDBOpenSuccess;
     request.onupgradeneeded = this.handleUpgradeNeededEvent;
@@ -45,6 +69,7 @@ export default {
     handleDBOpenSuccess: function (event) {
       this.db = event.target.result;
       this.dbOpen = true;
+      this.fetchAllGoals();
     },
 
     handleUpgradeNeededEvent: function (event) {
@@ -59,12 +84,14 @@ export default {
       let objStore = this.db.transaction('goals', 'readwrite').objectStore('goals');
       let request = objStore.add(this.goal);
 
-      request.onsuccess = function (event) {
-        console.log('Added successfully');
+      request.onsuccess = event => {
+        this.thisWeeksGoals.push({ goalName: this.goal.goalName, goalDate: this.goal.goalDate });
+        this.goal.goalName = '';
+        this.goal.goalDate = new Date();
       }
 
 
-      request.error = function (event) {
+      request.onerror = function (event) {
         console.group();
         console.error('Error adding to IndexedDB');
         console.error(event);
@@ -73,23 +100,16 @@ export default {
 
     },
 
-    addCustomer: function () {
-      let customerObjectStore = this.db.transaction("customers", "readwrite").objectStore("customers");
-      let request = customerObjectStore.add({ssn: 12345, name: 'Matt', email: 'test@example.com'});
+    fetchAllGoals: function () {
+      let transaction = this.db.transaction(["goals"]);
+      let objectStore = transaction.objectStore("goals");
+      let request = objectStore.getAll();
 
-      request.onsuccess = function (event) {
-        console.log('Added to db successfully');
-      }
-
-      request.onerror = function (event) {
-        console.group();
-        console.log('Error adding to object store');
-        console.log(event);
-        console.groupEnd();
-      }
-
+      request.onsuccess = event => {
+        console.log(request.result);
+        this.thisWeeksGoals = this.thisWeeksGoals.concat(request.result);
+      };
     }
-
   }
 }
 </script>
@@ -106,16 +126,6 @@ export default {
 
 h1, h2 {
   font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 
 a {
